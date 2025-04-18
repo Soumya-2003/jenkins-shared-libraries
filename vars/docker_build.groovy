688088a5a57446cf517ef2ae000ad776729459df
Soumyadeep Mallick <81796438+Soumya-2003@users.noreply.github.com>
2025-04-18 23:46:19 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
-def call(String ProjectName, String ImageTag, String DockerHub){
+def call(String ProjectName, String ImageTag, String DockerHubUser){
   echo "This is building the code"
   sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
